53463f28c1eb0a86ce000728f1996c4e5a745f7b
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-11 09:36:13 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbaity
+
+  def clickbaity
+    errors.add(:title, "must be clickbaity") unless title && title.match(/Won't Believe|Secret|Top [0-9]*|Guess/i)
+  end
 end
