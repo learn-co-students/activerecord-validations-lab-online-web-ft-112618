fc3a7c639b1f83e8f6e7150821cc936d1baa1817
Molly McCarron <mccarronmollye@gmail.com>
2019-02-11 09:38:55 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 10}
+  validates :summary, length: {maximum: 100}
+  validates :category, inclusion: { in: %w(Fiction Non-fiction)}
+  validate :clickbait
+end
+
+def clickbait
+  if !title.nil? && (!title.include?("You Won't" || "Secret" || "Top" || "Guess"))
+    errors.add(:clickbait, "That is not a clickbait worthy title. Try again.")
+  end
 end
